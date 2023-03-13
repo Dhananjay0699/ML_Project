31e4d29717e5dab02ef07d0b449283d12ca69f80
Dhananjay0699 <Dhananjayrb0699@gmail.com>
2023-03-13 15:02:16 +0530

    Data ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,7 +8,6 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
 
-
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
@@ -36,7 +35,7 @@ class DataIngestion:
 
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
-            logging.info("Ingestion of the data iss completed")
+            logging.info("Ingestion of the data is completed")
 
             return(
                 self.ingestion_config.train_data_path,
@@ -49,7 +48,3 @@ class DataIngestion:
 if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
-
-    data_transformation= DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
-
